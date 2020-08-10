ad1b9c10ee1f4ce1db261bbf02cb20c28c0b3412
mansi-958 <59793804+mansi-958@users.noreply.github.com>
2020-08-10 09:51:17 +0530

    Create Swap.py

---
+++ Task1/Swap.py
@@ -0,0 +1,9 @@
+a=int(input("Enter one number: "))
+b=int(input("Enter other number: "))
+print("Before swap")
+print("value of a ",a,"value of b ",b)
+a=a+b
+b=a-b
+a=a-b
+print("After swap")
+print("value of a ",a,"value of b ",b)
